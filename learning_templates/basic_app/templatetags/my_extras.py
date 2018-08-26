eb6ba46536b6a2deb811ca94de4f2f0768549fd1
parkerismus <parkerismus@gmail.com>
2018-08-26 17:25:13 -0400

    first

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.library()
+
+@register.filter(name='cutter')
+def cutter(value,arg):
+    """
+    This cuts out all values of "arg" from the string!
+    """
+    return value.replace(arg,'')
+
+# register.filter('cutter',cutter)
+# OR: use decorator @register.filter above
